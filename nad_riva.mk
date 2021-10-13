2c1e54a292ddb76bfb50ed5f1ca6d32314626f5f
ItoRenz <ridwan.ri214@gmail.com>
2021-10-13 18:19:27 +0700

    riva: update build fingerprint to redfin october
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>

--- nad_riva.mk
+++ nad_riva.mk
@@ -41,6 +41,6 @@ TARGET_VENDOR_PRODUCT_NAME := riva
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="riva" \
     PRODUCT_NAME="riva" \
-    PRIVATE_BUILD_DESC="riva-user 7.1.2 N2G47H V9.5.6.0.NCKMIFA release-keys"
+    PRIVATE_BUILD_DESC="google-user 11 RQ3A.211001.001 7641976 release-keys"
 
-BUILD_FINGERPRINT := Xiaomi/riva/riva:7.1.2/N2G47H/V9.5.6.0.NCKMIFA:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys
